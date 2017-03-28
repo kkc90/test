3bce04e7a37b2c944869e3b4d46e1d05cfc320ca
kkc90 <kc8070@naver.com>
2017-03-29 04:24:46 +0900

    second commit

--- Test.java
+++ Test.java
@@ -1,9 +1,18 @@
+import java.util.*;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=n; i++){
+			sb.append(i+"\n");
+		}
+		
+		System.out.print(sb);
+		
 	}
 
 }
